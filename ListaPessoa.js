28cd2cdf51d94bf28144744130d4c90009b56c16
felipescht709 <156364363+felipescht709@users.noreply.github.com>
2025-06-03 21:20:17 -0300

    Questão 03
    Requisito funcional - Usuários
    O sistema deve apresentar a lista de registros cadastrados e possibilitar a inserção, alteração e exclusão de registros.
    Listagem
    - Botão Adicionar, deve abrir formulário de cadastro em modo de inserção.
    - Na listagem de registros apresente:
     * Nome do usuário.
     * E-mail do usuário.
     * Telefone do usuário.
     * Botão de acesso para o formulário de cadastro em modo de edição, utilize o campo da chave primária como
    parâmetro para abertura do formulário.
    Formulário de cadastro
    Piway.com.br - Rafaeli Pinheiro - Todos os direitos reservados. Página 3/8
    - Campo de texto para o nome do usuário.
    - Campo de texto para o e-mail do usuário.
    - Campo de texto para o telefone do usuário.
    - Demais campos da tabela...
    - Botão Salvar, deve inserir ou alterar conforme a operação que abriu o formulário e voltar à página de listagem.
    - Botão Cancelar, deve cancelar a operação, voltando à página de listagem.
    - Botão Excluir, deve excluir o registro e voltar à página de listagem. Este botão somente deve ser exibido no modo de
    edição.

---
+++ ListaPessoa.js
@@ -0,0 +1,54 @@
+import TituloLista from "../componentes/TituloLista";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export default function ListaPessoa() {
+    const [dados, setDados] = useState([]);
+
+    const listar = async () => {
+        let { data } = await axios.get('http://localhost:4000/usuario');
+        setDados(data);
+    };
+
+    useEffect(() => {
+        listar();
+    }, []);
+
+    return (
+        <>
+            <TituloLista
+                titulo="Usuários"
+                descricao="Gerencie aqui os usuários do sistema"
+                rota="/cadastropessoa"
+            />
+            <div className="row">
+                <div className="col">
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Id</th>
+                                <th>Nome</th>
+                                <th>E-mail</th>
+                                <th>Telefone</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {dados.map((d) => (
+                                <tr key={d.idusuario}>
+                                    <td>
+                                        <a className="btn btn-primary" href={`/cadastropessoa/${d.idusuario}`}>Alterar</a>
+                                    </td>
+                                    <td>{d.idusuario}</td>
+                                    <td>{d.nomeusuario || d.nome}</td>
+                                    <td>{d.email}</td>
+                                    <td>{d.telefone}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </>
+    );
+}
